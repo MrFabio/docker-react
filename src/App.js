0325d651e5be025b20e0442cfc36af6309ef795d
Fábio Gomes <jgomesfa@jci.com>
2019-03-11 13:02:38 +0000

    First Commit

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn React se eu quiser
           </a>
         </header>
       </div>
